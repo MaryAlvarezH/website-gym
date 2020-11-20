876239a5528c44951d37d673e582a6fcf1f49dc7
MaryAlvarezH <maria.alvarez9409@gmail.com>
2020-11-20 17:52:30 -0600

    update content and styles of list-classes

--- inc/queries.php
+++ inc/queries.php
@@ -1,25 +1,50 @@
 <?php
     function gymfitness_lista_clases() {?>
         <ul class="list-classes">
-            <?php
-                // https://developer.wordpress.org/reference/classes/wp_query/
-                $args = array(
-                    'post_type' => 'gymfitness_clases',
-                    'post_per_page' =>10,
-                    'order' => 'ASC',
-                    'orderby' => 'title'
-                );
+        <?php
+            // https://developer.wordpress.org/reference/classes/wp_query/
+            $args = array(
+                'post_type' => 'gymfitness_clases',
+                'post_per_page' =>10,
+                'order' => 'ASC',
+                'orderby' => 'title'
+            );
 
-                $clases = new WP_Query($args);
+            $clases = new WP_Query($args);
 
-                while( $clases->have_posts() ): $clases->the_post();
-            ?>
-                    <li>
-                        <h1><?php the_title(); ?></h1>
-                    </li>
+            while( $clases->have_posts() ): $clases->the_post();
+        ?>
+                <li class="class card gradient">
+                    <?php the_post_thumbnail('median')?>
+                    <div class="container">
+                        <a href="<?php the_permalink(); ?>">
+                            <h3><?php the_title(); ?></h3>
+                        </a>
 
-                <?php endwhile; wp_reset_postdata(); ?>
+                        <?php
+                            $init_date = get_field('hora_inicio');
+                            $end_date = get_field('hora_fin');
+                        ?>
+
+                        <p>
+                            <?php the_field('dias_clase'); ?> - <?php echo $init_date . " a " . $end_date; ?>
+                        </p>
+                    </div>
+                </li>
+
+            <?php endwhile; wp_reset_postdata(); ?>
         </ul>
     <?php
     }
+
+    /* 
+    - Template tags en wordPress: the_post_thumbnail, the_permalink, the_title, etc 
+                                  tienen una función con el prefijo get_ por si se requiere asignar el valor a una variable
+                                  get_the_post_thumbnail
+
+    - Advance custom fields: the_flied('name')
+                            tienen una función con el prefijo get_  enb lugar de the_ por si se requiere asignar el valor a una variable
+                            get_flied('name')
+                            */
+                                    
 ?>
--- page-nuestras-clases.php
+++ page-nuestras-clases.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main class="container page section no-sidebar">
-    <div class="main-container text-center">
+    <div class="text-center">
         <?php get_template_part('template-parts/loop-pages')?>
         <?php gymfitness_lista_clases(); ?>
     </div>
