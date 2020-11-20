48b31b8f652fcfae8a97c9bffd56b1947faa09f5
MaryAlvarezH <maria.alvarez9409@gmail.com>
2020-11-20 12:16:04 -0600

    create a template with slug

---
+++ page-nuestras-clases.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main class="container page section no-sidebar">
+    <div class="main-container text-center">
+        <?php get_template_part('template-parts/loop-pages')?>
+    </div>
+</main>
+
+<?php get_footer();?>
