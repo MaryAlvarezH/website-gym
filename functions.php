08a122ea6b62bf9558fea5201633d6e0faeb753f
MaryAlvarezH <maria.alvarez9409@gmail.com>
2020-11-20 11:28:38 -0600

    create nosotros page

--- functions.php
+++ functions.php
@@ -1,17 +1,23 @@
-<!-- Funciones personalizadas disponibles en todo el tema, preferentemente acompañadas de un prefijo  -->
+
 
 <?php
+    // Funciones personalizadas disponibles en todo el tema, preferentemente acompañadas de un prefijo
 
-    //WHEN THEME IS ACTIVATED
+    // WHEN THEME IS ACTIVATED
     function gymfitness_setup() {
         // enable featured images
         add_theme_support('post-thumbnails');
 
+        // add custom size images
+        add_image_size( 'square', 350, 350, true );
+        add_image_size( 'portrait', 350, 724, true );
+        add_image_size( 'boxes', 400, 375, true );
+        add_image_size( 'median', 700, 400, true );
+        add_image_size( 'blog', 966, 644, true );
     }
-    add_action( 'after_setup_theme', 'gymfitness_setup' );
-
 
     // NAVIGATION MENUS
+    add_action( 'after_setup_theme', 'gymfitness_setup' );
     function gymfitness_menus(){
         $locations = array(
             'menu_principal' => __('Menu Principal', 'gymfitness'),
@@ -19,16 +25,15 @@
         );
         register_nav_menus( $locations );
     }
-
     add_action( 'init', 'gymfitness_menus' );
-    /* si uso __ significa que el texto se puede traducir
+    /* el __ significa que el texto se puede traducir
     hooks funciones que corren en determninado tiempo y lugar
     init -> la unción se ejecutará cuando wordpress se inicialice
     Se pueden agregar más menus utilizando el arreglo */
 
+
     // SCRIPS AND STYLES
     function gymfitness_scripts_styles() {
-        // hoja de estilos principal
         wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1' );
         wp_enqueue_style( 'slicknavCSS', 'https://cdnjs.cloudflare.com/ajax/libs/SlickNav/1.0.10/slicknav.min.css', array(), '1.0.10' );
         wp_enqueue_style( 'googleFont', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900|Staatliches&display=swap', array(), '1.0.0' );
@@ -38,10 +43,9 @@
 
     }
     add_action( 'wp_enqueue_scripts', 'gymfitness_scripts_styles' );
-
     /* cargar hojas de estilos en la parte frontal del sitio web:
     este hook esta almacenado dentro de otra función de wordPress wp_head
     por lo que es importante mandarla a llamar desde el head de nuestro .html
     get_stylesheet_uri() para obtener la uri de la hoja de estilos principal 
     el tercer parametro recibe un arreglo de dependencias que necesitan cargarse antes */
-?>:
+?>
--- page.php
+++ page.php
@@ -1,9 +1,31 @@
 <?php get_header(); ?>
 
-<?php while( have_posts() ): the_post();?>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-<?php endwhile; ?>
+<main class="container page section with-sidebar">
+    <div class="main-container">
+        <?php while( have_posts() ): the_post();?>
+            <h1 class="text-center primary-text">
+                <?php the_title(); ?>
+            </h1>
 
+            <?php 
+                if( has_post_thumbnail() ):
+                    the_post_thumbnail('blog', array('class' => 'featured-image'));
+                    
+                endif;
+
+                /* Default sizes param in the_post_thumbnail: 
+                thumbnail: 150px
+                medium: max 300px
+                large: max 1024px
+                full: original image size
+                */
+            ?>
+
+            <?php the_content(); ?>
+        <?php endwhile; ?>
+    </div>
+
+   <?php get_sidebar(); ?>
+</main>
 
 <?php get_footer();?>
